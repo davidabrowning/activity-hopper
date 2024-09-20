5a768b1b00fb7416e2a24324d7a343ac99cc10a2
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-20 16:58:00 +0200

    Added ActivityHopper.java file.

---
+++ activity-hopper/src/main/java/com/example/domain/ActivityHopper.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Holds a collection of Activities
+ */
+public class ActivityHopper {
+    
+    // Instance variables
+    List<Activity> activitiesList;
+
+    public ActivityHopper() {
+        activitiesList = new ArrayList<>();
+    }
+
+}
