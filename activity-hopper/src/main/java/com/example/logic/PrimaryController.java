c2d88a6d5074b8a85410b9048d30cdbd4aceb0d6
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-21 16:08:19 +0200

    Added Controllers to a ui folder.

---
+++ activity-hopper/src/main/java/com/example/logic/PrimaryController.java
@@ -0,0 +1,15 @@
+package com.example.logic;
+
+import java.io.IOException;
+
+import com.example.ui.App;
+
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ activity-hopper/src/main/java/com/example/logic/SecondaryController.java
@@ -0,0 +1,15 @@
+package com.example.logic;
+
+import java.io.IOException;
+
+import com.example.ui.App;
+
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
